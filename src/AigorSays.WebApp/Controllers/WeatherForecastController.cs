13f25f53d93d2ccb23618d2a712ab813ee9cc770
francho <lab@francho.org>
2020-07-18 19:28:26 +0200

    Add project WebApp

--- src/AigorSays.WebApp/Controllers/WeatherForecastController.cs
+++ src/AigorSays.WebApp/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace AigorSays.Host.Controllers
+namespace AigorSays.WebApp.Controllers
 {
     [ApiController]
     [Route("[controller]")]
